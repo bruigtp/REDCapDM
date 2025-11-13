2c3af35c3161c017f3d424bdb33e336cdb631c73
jcarmezim <jcarmezim@igtp.cat>
2025-11-13 19:19:28 +0100

    Updating information

--- R/REDCapDM-package.R
+++ R/REDCapDM-package.R
@@ -19,17 +19,17 @@
 #'
 #' Core Functions:
 #'
-#' - `redcap_data`: Reads data exported from REDCap or retrieved through the REDCap API.
-#' - `rd_transform`: Processes raw REDCap datasets into a structured and analyzable format.
-#'    - `transform_dates`: Transform dates and datetimes variables.
-#'    - `recalculate`: Recalculates REDCap calculated fields, compares them to originals, and reports discrepancies.
-#'    - `to_factor`: Converts variables to factors and updates the dictionary's branching logic.
-#'    - `rd_delete_vars`: Deletes specified or pattern-matched variables from the data and dictionary.
-#'    - `transform_checkbox`: Transforms the names of REDCap checkbox variables and updates the branching logic in the dictionary.
-#'    - `transform_dic`: Evaluates and transforms branching logic in the REDCap dictionary into R logic.
+#' - `redcap_data`: Reads data exported from REDCap or retrieved through the REDCap API into R.
+#' - `rd_transform`: One-step pipeline to clean and preprocess the raw REDCap data.
+#'    - `rd_dates`: Standardize date and datetime fields.
+#'    - `rd_delete_vars`: Remove specified variables (by name or pattern).
+#'    - `rd_recalculate`: Recompute calculated fields and compare with REDCap values.
+#'    - `rd_factor`: Replace numeric multiple-choice columns with their factor version.
+#'    - `rd_checkbox`: Expand checkbox responses with custom labels and rename 'var___1' columns (REDCap style) to 'var_option'.
 #'    - `rd_split`: Splits a REDCap dataset by form or event.
-#' - `rd_rlogic`: Converts REDCap branching and conditional logic into R-compatible expressions.
-#' - `rd_insert_na`: Inserts missing values into specified variables based on filters.
+#'    - `rd_insert_na`: Manually set specified variables to missing based on a logical filter.
+#'    - `rd_rlogic`: Translate REDCap branching or calculation logic into R syntax.
+#'    - `rd_dictionary`: Update dictionary (translation of REDCap logic into R syntax) to reflect transformed data and logic.
 #' - `rd_query`: Identifies discrepancies (queries) in the dataset for validation.
 #' - `rd_event`: Detects missing events in longitudinal datasets.
 #' - `check_queries`: Compares historical and current query reports to track changes and additions.
